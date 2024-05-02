a5d939e26ff8e6d997e3db7408e6e4fb43f37d5e
webmsgr <zapcontact000@gmail.com>
2024-05-01 18:55:48 -0700

    add explicit types

--- src/index.ts
+++ src/index.ts
@@ -15,7 +15,7 @@ class StringFormItem {
     }
 
     validate(): boolean {
-        let val = this.#control.value;
+        let val: string = this.#control.value;
         if (val === ''){
             this.#errorLabel.innerText = 'Please enter a value';
             this.#valid = false;
@@ -58,7 +58,7 @@ class IntFormItem {
     }
 
     validate(): boolean {
-        let val = parseInt(this.#control.value);
+        let val: number = parseInt(this.#control.value);
         if (!is_number(this.#control.value) || val < 0 || this.#control.value.includes('.')) {
             this.#errorLabel.innerText = 'Please enter a valid number';
             this.#valid = false;
@@ -100,7 +100,7 @@ class DecimalFormItem {
     }
 
     validate(): boolean {
-        let val = parseFloat(this.#control.value);
+        let val: number = parseFloat(this.#control.value);
         if (!is_number(this.#control.value) || val < 0){
             this.#errorLabel.innerText = 'Please enter a valid number';
             this.#valid = false;
@@ -138,12 +138,12 @@ function is_number(val: string): boolean {
 }
 
 function main() {
-    let form = document.querySelector('form')!; // we know the form exists
-    let name = new StringFormItem(document.querySelector('#name') as HTMLInputElement, document.querySelector('#nameError')!);
-    let quantity = new IntFormItem(document.querySelector('#quantity') as HTMLInputElement, document.querySelector('#quantityError')!);
-    let price = new DecimalFormItem(document.querySelector('#price') as HTMLInputElement, document.querySelector('#priceError')!);
-    let tax = new DecimalFormItem(document.querySelector('#tax') as HTMLInputElement, document.querySelector('#taxError')!);
-    let result_box = document.querySelector('#result')!;
+    let form: HTMLFormElement = document.querySelector('form')!; // we know the form exists
+    let name: StringFormItem = new StringFormItem(document.querySelector('#name') as HTMLInputElement, document.querySelector('#nameError')!);
+    let quantity: IntFormItem = new IntFormItem(document.querySelector('#quantity') as HTMLInputElement, document.querySelector('#quantityError')!);
+    let price: DecimalFormItem = new DecimalFormItem(document.querySelector('#price') as HTMLInputElement, document.querySelector('#priceError')!);
+    let tax: DecimalFormItem = new DecimalFormItem(document.querySelector('#tax') as HTMLInputElement, document.querySelector('#taxError')!);
+    let result_box: HTMLElement = document.querySelector('#result')!;
     form.addEventListener('submit', (e) => {
         e.preventDefault();
         name.validate();
@@ -154,9 +154,9 @@ function main() {
             result_box.textContent = 'Please fix the errors above';
             return;
         }
-        let subtotal = quantity.value * price.value;
-        let total = subtotal + (subtotal * (tax.value/100.0))
-        let result_str = `'${name.value}' x ${quantity.value} @ $${price.value.toFixed(2)} = ${subtotal.toFixed(2)} + ${tax.value}% tax = ${total.toFixed(2)}`;
+        let subtotal: number = quantity.value * price.value;
+        let total: number = subtotal + (subtotal * (tax.value/100.0))
+        let result_str: string = `'${name.value}' x ${quantity.value} @ $${price.value.toFixed(2)} = ${subtotal.toFixed(2)} + ${tax.value}% tax = ${total.toFixed(2)}`;
         result_box.textContent = result_str;
     });
 }
